cbcd80370860d8e1a02a465a3160e10472f9ce80
user <user@MacBook-Air-de-user-4.local>
2024-12-12 15:38:51 -0500

    feat: preliminar search of Y position

--- player_example/index.ts
+++ player_example/index.ts
@@ -37,32 +37,74 @@ const getPlayerTheBallIsGoingTo = (previousX: number, currentX: number) => {
   return 2;
 }
 
+const directToDesiredY = (desiredY: number, currentPlayerY: number) => {
+  const delta = 20;
+  if(currentPlayerY < desiredY + delta && currentPlayerY > desiredY - delta){
+    return 0
+  }
+  else if(currentPlayerY < desiredY){
+    return 1
+  }
+  else
+    return -1
+}
+
+const calculateYCollision = (currentInput: input, previousInput: input): number => {
+  const ballDeltaX = currentInput.bx - previousInput.bx;
+  const ballDeltaY = currentInput.by - previousInput.by;
+  let expectedPreliminarCollision: number = -1;
+  let remainingX: number = -1;
+  
+  const limit : number = ballDeltaX < 0 ? 55 : 845;
+
+  remainingX = limit - currentInput.bx;
+  const stepsForCollision = Math.round(remainingX/ballDeltaX);
+  expectedPreliminarCollision = currentInput.by + (stepsForCollision * ballDeltaY);
+  console.log({expectedPreliminarCollision})
+  if(expectedPreliminarCollision > 600){
+    expectedPreliminarCollision = 1200 - expectedPreliminarCollision;
+  } else if (expectedPreliminarCollision < 0){
+    expectedPreliminarCollision = -expectedPreliminarCollision;
+  }
+  
+  return expectedPreliminarCollision;
+}
+
 const tickLogic = (currentInput: input, previousInput: input ) => {
   const delta = 5;
   let direction = 0;
 
   const ballDirectedTo = getPlayerTheBallIsGoingTo(previousInput.bx, currentInput.bx);
   
-  if (Number(currentInput.py) - delta > Number(currentInput.by)) {
-    direction = -1
+  if(ballDirectedTo === player){
+    const expectedCollision = calculateYCollision(currentInput, previousInput);
+    console.log({expectedCollision})
+    direction = directToDesiredY(expectedCollision, currentInput.py);
+/*    if (Number(currentInput.py) - delta > Number(currentInput.by)) {
+      direction = -1
+    }
+    if (Number(currentInput.py) + delta < Number(currentInput.by)) {
+      direction = 1
+    }
+*/
+  } else {
+    direction = directToDesiredY(300, currentInput.py);
   }
-  if (Number(currentInput.py) + delta < Number(currentInput.by)) {
-    direction = 1
-  }
-  console.log({direction, ballDirectedTo})
-  console.log(outputFile)
+
+//  console.log({direction, ballDirectedTo})
+//  console.log(outputFile)
   fs.writeFileSync(outputFile, `${direction}`, { "encoding": "utf-8" })
 }
 
 (async () => {
-  console.log({ inputFile, outputFile })
+//  console.log({ inputFile, outputFile })
   while (true) {
     const input = fs.readFileSync(inputFile, { encoding: "utf-8" })
-    const [px, py, e, bx, by] = input.split(",").map(s => { return s.trim() });
+    const [px, py, e, bx, by] = input.split(",").map(s => { return Number(s.trim()) });
     if(player === 0)
       player = getPlayer(px);
-    console.log({ px, py, e, bx, by, player })
-    if (previousInput[0] < 0 || Number(e) < 20) {
+//    console.log({ px, py, e, bx, by, player })
+    if (previousInput.px < 0 || Number(e) < 20) {
       console.log(Number(e) < 20)
       fs.writeFileSync(outputFile, "0", { "encoding": "utf-8" })
     } else {
